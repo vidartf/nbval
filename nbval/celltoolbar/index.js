d693ab3997c3299bc87be396abc63646307c315d
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-01-04 15:55:14 +0100

    Add toolbar dialog for extra sanitizers

--- nbval/celltoolbar/index.js
+++ nbval/celltoolbar/index.js
@@ -4,12 +4,69 @@
 
 define([
     'notebook/js/celltoolbar',
-], function(celltoolbar) {
+    'base/js/dialog',
+], function(celltoolbar, dialog) {
     "use strict";
 
     var CellToolbar = celltoolbar.CellToolbar;
 
     var nbval_preset = [];
+
+    function edit_sanitizers(cell) {
+        var message =
+            "Edit the cell specific sanitizers below, according to the specs for the " +
+            "sanitizer files.";
+        var ns = cell.metadata.nbval;
+        var sanitizers = (ns === undefined || ns.extra_sanitizers === undefined) ? '': ns.extra_sanitizers;
+        var textarea = $('<textarea/>')
+            .attr('rows', '13')
+            .attr('cols', '80')
+            .attr('name', 'sanitizers')
+            .text(sanitizers);
+
+        var dialogform = $('<div/>').attr('title', 'Edit the metadata')
+            .append(
+                $('<form/>').append(
+                    $('<fieldset/>').append(
+                        $('<label/>')
+                        .attr('for','sanitizers')
+                        .text(message)
+                        )
+                        .append($('<br/>'))
+                        .append(textarea)
+                    )
+            );
+        var modal_obj = dialog.modal({
+            title: "Edit cell nbval output sanitizers",
+            body: dialogform,
+            default_button: "Cancel",
+            buttons: {
+                Cancel: {},
+                Edit: { class : "btn-primary",
+                    click: function() {
+                        if (cell.metadata.nbval === undefined) {
+                            cell.metadata.nbval = {};
+                        }
+                        cell.metadata.nbval.extra_sanitizers = textarea[0].value;
+                    }
+                }
+            },
+            notebook: cell.notebook,
+            keyboard_manager: cell.keyboard_manager
+        });
+    }
+
+    var add_sanitizer_dialog_button = function(div, cell) {
+        var button_container = $(div);
+        var button = $('<button />')
+            .addClass('btn btn-default btn-xs')
+            .text('Extra Sanitizers')
+            .click( function() {
+              edit_sanitizers(cell);
+              return false;
+            });
+        button_container.append(button);
+    };
 
     var checkbox_test = CellToolbar.utils.checkbox_ui_generator('Check outputs',
          // setter
@@ -33,6 +90,9 @@ define([
 
 
     var load_ipython_extension = function () {
+        CellToolbar.register_callback('nbval.edit_sanitizers', add_sanitizer_dialog_button, ['code']);
+        nbval_preset.push('nbval.edit_sanitizers');
+
         CellToolbar.register_callback('nbval.compare_outputs', checkbox_test, ['code']);
         nbval_preset.push('nbval.compare_outputs');
 
